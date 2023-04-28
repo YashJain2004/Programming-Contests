9c8ab32f9e23c8e28e528781a6c63fc6cdeeaf4f
Yash Jain <jain.yash20082004@gmail.com>
2023-04-28 19:39:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ response.then((v)=>{
         ihtml += 
         `<div class="card my-2 mx-2" style="width: 22rem;" >
         <img src="${imgArr[number]}" style="height: 200px">
-          <h5 class="card-title">${contests[item].name.slice(0,15)} </h5>
+          <h5 class="card-title">${contests[item].name.slice(0,15)}... </h5>
           <p>Site is: ${contests[item].site}</p>
           <p>Status is: ${contests[item].status}</p>
           <p>In 24 Hours?: ${contests[item].in_24_hours}</p>
